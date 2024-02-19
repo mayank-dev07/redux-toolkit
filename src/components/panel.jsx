69cfca08e99a8213e1071cba82de4254f4006fdb
mayank-dev07 <devmayank.hts@gmail.com>
2024-02-19 19:55:26 +0530

    panel jsx deleted]

--- src/components/panel.jsx
+++
@@ -1,8 +0,0 @@
-export default function Panel({ title, theme }) {
-  const className = "bg-" + theme;
-  return (
-    <section className={className}>
-      <h1>{title}</h1>
-    </section>
-  );
-}
